196f0bb4ad3c5d4d6018a19ea9d4cfdaabeecba0
Major Hayden 🤠 <major@mhtx.net>
2022-05-06 12:05:50 -0500

    Add https:// in build script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ BRANCH=${CF_PAGES_BRANCH:-main}
 
 if [[ $BRANCH == "main" ]]; then
     # build using the public url
-    hugo -b major.io
+    hugo -b https://major.io
 else
     # build using cloudflare's temporary url
     hugo -b $CF_PAGES_URL
