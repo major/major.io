5d87c2bea12bb0abebacf5f9f84a79c6995d29f4
Major Hayden <major@mhtx.net>
2023-06-23 13:34:54 -0500

    Turn all posts into bundles

--- scripts/make-bundles.py
+++ scripts/make-bundles.py
@@ -8,8 +8,9 @@ markdown_path = sys.argv[1]
 
 markdown_files = glob(f"{markdown_path}/*.md")
 for markdown_file in markdown_files:
-    slug, _ = markdown_file.split(".")
-
+    slug = markdown_file.split("/")[-1].split(".")[0]
     bundle_dir = f"{markdown_path}/{slug}/"
-    os.makedirs(bundle_dir, exist_ok=True)
-    os.rename(markdown_file, f"{bundle_dir}/index.md")
+    bundle_file = f"{bundle_dir}/index.md"
+
+    os.makedirs(bundle_dir)
+    os.rename(markdown_file, bundle_file)
