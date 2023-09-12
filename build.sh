262249cd65265835791843cc96bda6f29c1da336
Major Hayden <major@mhtx.net>
2023-09-12 10:36:45 -0500

    Update build script to use latest hugo version

--- build.sh
+++ build.sh
@@ -2,12 +2,19 @@
 # Build the site properly in Cloudflare pages.
 set -euxo pipefail
 
+# Determine our current branch.
 BRANCH=${CF_PAGES_BRANCH:-main}
+
+# Get the latest hugo version from the go.mod
+LATEST_HUGO_VERSION=$(grep github.com/gohugoio/hugo go.mod | awk '{print $NF}' | sed 's/^v//')
+HUGO_DOWNLOAD_URL="https://github.com/gohugoio/hugo/releases/download/v${LATEST_HUGO_VERSION}/hugo_extended_${LATEST_HUGO_VERSION}_linux-amd64.tar.gz"
+curl -sL --retry 5 "$HUGO_DOWNLOAD_URL" | tar xvzf - hugo
+./hugo version
 
 if [[ $BRANCH == "main" ]]; then
     # build using the public url
-    hugo -b https://major.io
+    ./hugo -b https://major.io
 else
     # build using cloudflare's temporary url
-    hugo -b $CF_PAGES_URL
+    ./hugo -b "$CF_PAGES_URL"
 fi
---
+++ go.mod
@@ -0,0 +1,5 @@
+module major.io
+
+go 1.20
+
+require github.com/gohugoio/hugo v0.118.2
