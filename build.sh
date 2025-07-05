82d3b2b1ad7204bdf34a38eaabfaecf6069ca6a4
Major Hayden <major@mhtx.net>
2025-07-05 11:40:33 -0500

    Remove base url, add redirects

--- build.sh
+++ build.sh
@@ -2,22 +2,10 @@
 # Build the site properly in Cloudflare pages.
 set -euxo pipefail
 
-# Dump the environment variables for debugging.
-env | sort
-
-# Determine our current branch.
-BRANCH=${CF_PAGES_BRANCH:-main}
-
 # Get the latest hugo version from the go.mod
 LATEST_HUGO_VERSION=$(grep github.com/gohugoio/hugo go.mod | awk '{print $NF}' | sed 's/^v//')
 HUGO_DOWNLOAD_URL="https://github.com/gohugoio/hugo/releases/download/v${LATEST_HUGO_VERSION}/hugo_extended_${LATEST_HUGO_VERSION}_linux-amd64.tar.gz"
 curl -sL --retry 5 "$HUGO_DOWNLOAD_URL" | tar xvzf - hugo
 ./hugo version
 
-if [[ $BRANCH == "main" ]]; then
-    # build using the public url
-    ./hugo -b https://major.io
-else
-    # build using cloudflare's temporary url
-    ./hugo -b "$CF_PAGES_URL"
-fi
+./hugo --enableGitInfo --minify --cleanDestinationDir --destination public
